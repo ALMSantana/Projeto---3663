3b4f04601ad668bf4d9e98212d6d3245d333a955
André Santana <andrelms4@insper.edu.br>
2024-02-15 10:08:10 -0300

    Aula 3-2

--- assistente_projeto.py
+++ assistente_projeto.py
@@ -7,6 +7,49 @@ load_dotenv()
 
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def apagar_assistente(assistant_id):
+    cliente.beta.assistants.delete(assistant_id)
+
+def apagar_thread(thread_id):
+    cliente.beta.threads.delete(thread_id)
+
+def apagar_arquivos(lista_ids_arquivos):
+    for um_id in lista_ids_arquivos:
+        cliente.files.delete(um_id)
+
+def criar_thread():
+    return cliente.beta.threads.create()
+
+def criar_assistente(lista_ids_arquivos=[], modelo =MODELO_GPT_4):
+    assistente = cliente.beta.assistants.create(
+        name="Atendente Eng. Software",
+        instructions = f"""
+            Assuma que você é um assistente virtual especializado em orientar desenvolvedores e QA testers na criação de testes automatizados para aplicações web usando Python e Selenium. 
+            
+            Você deve oferecer suporte abrangente, desde o setup inicial do ambiente de desenvolvimento até a implementação 
+            de testes complexos, adotando e consultando principalmente os documentos de sua
+            base (para identificar padrões e formas de estruturar os scripts solicitados).
+
+            Consulte sempre os arquivos html, css e js para elaborar um teste.
+            
+            Adicionalmente, você deve ser capaz de explicar conceitos chave de 
+            testes automatizados e Selenium, fornecer templates de código personalizáveis, e oferecer feedback sobre scripts de teste escritos pelo usuário. 
+
+            O objetivo é facilitar o aprendizado e a aplicação de testes automatizados, 
+            melhorando a qualidade e a confiabilidade das aplicações web desenvolvidas.
+
+            Caso solicitado a gerar um script, apenas gere ele sem outros comentários adicionais.
+
+            Você também é um especialista em casos de uso, seguindo os templates indicados.
+            E também é um especialista em gerar cenários de teste.
+                """,
+        model = modelo,
+        tools=  [{"type": "retrieval"}],
+        file_ids = lista_ids_arquivos
+    )
+
+    return assistente
+
 def criar_lista_ids_app_web(diretorio = "AcordeLab"):
     lista_ids_arquivos = []
     dicionario_arquivos = {}
